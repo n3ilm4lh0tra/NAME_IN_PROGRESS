ed40e3379a38ee6699490bb990b5b849d59c465d
n3ilm4lh0tra <malhotra@spsejecna.cz>
2024-05-04 16:21:09 +0200

    added height and width

--- src/Panel.java
+++ src/Panel.java
@@ -1,2 +1,12 @@
-public class Panel {
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel extends JPanel {
+    public int HEIGHT = 1100;
+    public int WIDTH = 800;
+
+    public Panel() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setBackground(Color.BLACK);
+    }
 }
