f99461ee25cdbbb1cf779fd95beec41d9f3eeee9
n3ilm4lh0tra <malhotra@spsejecna.cz>
2024-05-04 16:31:30 +0200

    added FPS and thread

--- src/Panel.java
+++ src/Panel.java
@@ -4,6 +4,8 @@ import java.awt.*;
 public class Panel extends JPanel {
     public int WIDTH = 1100;
     public int HEIGHT = 800;
+    final int FPS = 60;
+    Thread thread;
 
     public Panel() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
