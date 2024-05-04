308f06406eed1b181733b906a059a9450719a12e
n3ilm4lh0tra <malhotra@spsejecna.cz>
2024-05-04 16:41:08 +0200

    added frame update system in run method

--- src/Panel.java
+++ src/Panel.java
@@ -17,6 +17,23 @@ public class Panel extends JPanel implements Runnable{
     }
     @Override
     public void run() {
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while(thread != null){
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+            }
+        }
     }
     private void update(){}
     public void paintComponent(Graphics g){}
