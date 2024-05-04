ff5f0ced6dbe442fe724217ea258709669840f29
n3ilm4lh0tra <malhotra@spsejecna.cz>
2024-05-04 16:13:55 +0200

    Made Panel.java Created window in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame window = new JFrame("NAME_IN_PROGRESS");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(true);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
     }
 }
