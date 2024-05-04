494a4e7358de04e7f5e4ccc5b08ce7e006027d3d
n3ilm4lh0tra <malhotra@spsejecna.cz>
2024-05-04 16:28:06 +0200

    switching HEIGHT and WIDTH

--- src/Panel.java
+++ src/Panel.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Panel extends JPanel {
-    public int HEIGHT = 1100;
-    public int WIDTH = 800;
+    public int WIDTH = 1100;
+    public int HEIGHT = 800;
 
     public Panel() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
