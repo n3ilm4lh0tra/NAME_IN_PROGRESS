1f345316cfd9746bdd34d338a90596b27176a269
n3ilm4lh0tra <126697960+n3ilm4lh0tra@users.noreply.github.com>
2024-05-15 11:01:00 +0200

    Update Objects.java
    Added add method

--- src/Objects.java
+++ src/Objects.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Objects(){
     ArrayList<Object> o = new ArrayList<>();
+    int index = 0;
     public Objects(){}
     public void addObject(){
         Scanner sc = new Scanner(System.in);
@@ -9,5 +10,14 @@ public class Objects(){
         String name = sc.nextLine();
         System.out.print("object width:\n#");
         int width = sc.nextInt();
+        System.out.print("object height:\n#");
+        int height = sc.nextInt();
+        System.out.print("object x:\n#");
+        int x = sc.nextInt();
+        System.out.print("object y:\n#");
+        int y = sc.nextInt();
+        o.add(i, new Object(name, width, height, x, y));
+        i++;
+        System.out.println("object added");
     }
 }
