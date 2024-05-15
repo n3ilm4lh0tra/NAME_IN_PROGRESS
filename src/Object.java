55c74874f242f5f2eadd5b882d8b76633c8a4b38
n3ilm4lh0tra <126697960+n3ilm4lh0tra@users.noreply.github.com>
2024-05-15 08:13:12 +0200

    Added Object.java
    Object has a name, width, height and coordinates on the x and y axis.

---
+++ src/Object.java
@@ -0,0 +1,15 @@
+public class Object(){
+    String name;
+    int width;
+    int height;
+    int x;
+    int y;
+
+    public Object(Sting name, int width, int height, int x, int y){
+        this.name=name;
+        this.width=width;
+        this.height=height;
+        this.x=x;
+        this.y=y;
+    }
+}
