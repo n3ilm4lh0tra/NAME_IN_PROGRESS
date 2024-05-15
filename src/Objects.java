17fe286826dc02834e90b131ae8097c9466e6345
n3ilm4lh0tra <126697960+n3ilm4lh0tra@users.noreply.github.com>
2024-05-15 08:47:42 +0200

    Update Objects.java

--- src/Objects.java
+++ src/Objects.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Objects(){
-    
+    ArrayList<Object> o = new ArrayList<>();
+    public Objects(){}
+    public void addObject(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("object name:\n#");
+        String name = sc.nextLine();
+        System.out.print("object width:\n#");
+        int width = sc.nextInt();
+    }
 }
