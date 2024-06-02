23533ac56552af7b932f8f59ed779451376b8ca7
n3ilm4lh0tra <malhotra@spsejecna.cz>
2024-06-02 14:49:07 +0200

    adding junit

---
+++ src/ObjectsTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ObjectsTest {
+
+    @Test
+    void addObject() {
+    }
+
+    @Test
+    void checkBounds() {
+    }
+
+    @Test
+    void dupeChecker() {
+    }
+
+    @Test
+    void checkCollision() {
+    }
+
+    @Test
+    void checkColour() {
+    }
+
+    @Test
+    void update() {
+    }
+}
